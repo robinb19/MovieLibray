4071c9906d94a4ec765012846152e37db22dbc7d
robinb19 <111273003+robinb19@users.noreply.github.com>
2023-04-12 14:51:54 +0200

    Add files via upload

---
+++ MovieLibray/Pages/Team.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MovieLibrary.Pages
+{
+    public class TeamModel : PageModel
+    {
+        private readonly ILogger<TeamModel> _logger;
+
+        public TeamModel(ILogger<TeamModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
